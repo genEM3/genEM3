e731878dd953438699d693307488358e190671da
Ali Karimi <ali.karimi@brain.mpg.de>
2020-05-30 14:06:30 +0200

    Try running the trainer_test script

---
+++ playground/AK/trainer_test/main.py
@@ -0,0 +1,74 @@
+import os
+import time
+import torch
+import numpy as np
+from matplotlib import pyplot as plt
+from torch.utils.data import DataLoader
+
+from genEM3.data.wkwdata import WkwData
+from genEM3.model.autoencoder2d import AE, Encoder_4_sampling_bn, Decoder_4_sampling_bn
+from genEM3.training.training import Trainer
+import pdb
+pdb.set_trace()
+from genEM3.util import gpu
+gpu.get_empty_gpu() 
+# Parameters
+run_root = os.path.dirname(os.path.abspath(__file__))
+
+wkw_root = '/tmpscratch/webknossos/Connectomics_Department/' \
+                  '2018-11-13_scMS109_1to7199_v01_l4_06_24_fixed_mag8/color/1'
+
+cache_root = os.path.join(run_root, '.cache/')
+datasources_json_path = os.path.join(run_root, 'datasources.json')
+data_strata = {'training': [1, 2], 'validate': [3], 'test': []}
+input_shape = (302, 302, 1)
+output_shape = (302, 302, 1)
+norm_mean = 148.0
+norm_std = 36.0
+
+# Run
+data_sources = WkwData.datasources_from_json(datasources_json_path)
+
+# With Caching (cache filled)
+dataset = WkwData(
+    data_sources=data_sources,
+    data_strata=data_strata,
+    input_shape=input_shape,
+    output_shape=output_shape,
+    norm_mean=norm_mean,
+    norm_std=norm_std,
+    cache_root=cache_root,
+    cache_size=1024,#MiB
+    cache_dim=2,
+    cache_range=8
+)
+
+dataloader = DataLoader(dataset, batch_size=8, shuffle=False, num_workers=0)
+
+input_size = 302
+output_size = input_size
+valid_size = 17
+kernel_size = 3
+stride = 1
+n_fmaps = 8
+n_latent = 5000
+model = AE(
+    Encoder_4_sampling_bn(input_size, kernel_size, stride, n_fmaps, n_latent),
+    Decoder_4_sampling_bn(output_size, kernel_size, stride, n_fmaps, n_latent))
+criterion = torch.nn.MSELoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.8)
+
+num_epoch = 10
+log_int = 5
+device = 'gpu'
+
+trainer = Trainer(run_root,
+                 dataloader,
+                 model,
+                 optimizer,
+                 criterion,
+                 num_epoch,
+                 log_int,
+                 device)
+
+trainer.train()
