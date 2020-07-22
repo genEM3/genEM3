acdd166bd034feef0fa0b36b956aff47e15a36fb
Florian Drawitsch <florian.drawitsch@gmail.com>
2020-07-22 20:41:53 +0200

    after successful training of ae_03

--- runs/ae_v03/main.py
+++ runs/ae_v03/main.py
@@ -19,8 +19,8 @@ data_split = DataSplit(train=0.75, validation=0.15, test=0.1)
 cache_RAM = True
 cache_HDD = False
 cache_root = os.path.join(run_root, '.cache/')
-batch_size = 32
-num_workers = 4
+batch_size = 128
+num_workers = 8
 
 dataset = WkwData(
     input_shape=input_shape,
@@ -58,7 +58,7 @@ model = AE(
 criterion = torch.nn.MSELoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=0.025, momentum=0.8)
 
-num_epoch = 50
+num_epoch = 100
 log_int = 100
 device = 'cuda'
 save = True
