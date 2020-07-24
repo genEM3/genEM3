482025961b2492bc17356b6119cf142d7b847374
Ali Karimi <ali.karimi@brain.mpg.de>
2020-07-24 16:13:10 +0200

    Created the data loader directly from the numpy array

--- playground/AK/trainingDataRead/readData.py
+++ playground/AK/trainingDataRead/readData.py
@@ -3,7 +3,7 @@ import os
 import numpy as np
 from genEM3.data.wkwdata import WkwData
 from matplotlib import pyplot as plt
-
+import torch
 # Read the nml and print some basic properties
 nmlDir = '/gaba/u/alik/code/genEM3/playground/AK/trainingDataRead'
 nmlName = 'artefact_trainingData.nml'
@@ -25,15 +25,30 @@ def bboxFromCenter(center, dims):
     # make sure it is only a single slice (3rd dim size = 0)
     return np.hstack([topLeft, dims[0:2], np.ones(1)]).astype(int)
 
+
 # Get the bounding boxes of each debris location
 curDims = np.array([140, 140, 0])
-bboxFromCenterFixedDim = lambda coord: bboxFromCenter(coord,curDims)
+bboxFromCenterFixedDim = lambda coord: bboxFromCenter(coord, curDims)
 bboxes = np.apply_along_axis(bboxFromCenterFixedDim, 1, coordArray)
 # read the wkwdata into a numpy array
 wkwDir = '/tmpscratch/webknossos/Connectomics_Department/2018-11-13_scMS109_1to7199_v01_l4_06_24_fixed_mag8/color/1'
 readWk = lambda bbox: WkwData.wkw_read(wkwDir, bbox)
-images = np.apply_along_axis(readWk, 1, bboxes).squeeze(4)
-for i in range(111):
-    print(f'bbox {i}:{bboxes[i,:]}')
-    plt.imshow(np.squeeze(images[i, 0, :, :]), cmap='gray')
-    plt.show()
+images = np.apply_along_axis(readWk, 1, bboxes).squeeze(4).astype('double')
+# normalize images
+imagesNormal = (images-148)/36
+# Create pytorch dataset from numpy array
+imgT = torch.Tensor(imagesNormal)
+dataset = torch.utils.data.TensorDataset(imgT)
+dataLoader = torch.utils.data.DataLoader(dataset)
+
+
+showExamples = False
+if showExamples:
+    for i in range(111):
+        print(f'bbox {i}:{bboxes[i,:]}')
+        plt.imshow(np.squeeze(images[i, 0, :, :]), cmap='gray')
+        plt.show()
+
+    for i, example in enumerate(dataLoader):
+        plt.imshow(np.squeeze(example[0].numpy()), cmap='gray')
+        plt.show()
