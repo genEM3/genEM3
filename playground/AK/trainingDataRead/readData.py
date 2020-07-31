e32bbe7b5919c7149ea6c78a11f34b99ca279bec
Ali Karimi <ali.karimi@brain.mpg.de>
2020-07-31 15:23:40 +0200

    Run the PCA on larger model

--- playground/AK/trainingDataRead/readData.py
+++ playground/AK/trainingDataRead/readData.py
@@ -8,7 +8,7 @@ from sklearn import decomposition
 
 from genEM3.data.wkwdata import WkwData
 from genEM3.data.skeleton import getAllTreeCoordinates
-from genEM3.model.autoencoder2d import AE, Encoder_4_sampling_bn_1px_deep, Decoder_4_sampling_bn_1px_deep
+from genEM3.model.autoencoder2d import AE, Encoder_4_sampling_bn_1px_deep_convonly_skip, Decoder_4_sampling_bn_1px_deep_convonly_skip
 from genEM3.inference.inference import Predictor
 from genEM3.util.image import normalize, readWkwFromCenter
 # seed numpy random number generator
@@ -60,7 +60,10 @@ dataset = WkwData(
     input_shape=input_shape,
     target_shape=output_shape,
     data_sources=datasources,
+    cache_HDD=False,
+    cache_RAM=True,
 )
+dataset[0]
 clean_loader = torch.utils.data.DataLoader(
     dataset=dataset, batch_size=batch_size, num_workers=num_workers)
 # settings for the model to be loaded
@@ -70,12 +73,12 @@ device = 'cpu'
 kernel_size = 3
 stride = 1
 n_fmaps = 16
-n_latent = 192
+n_latent = 2048
 input_size = 140
 output_size = input_size
 model = AE(
-    Encoder_4_sampling_bn_1px_deep(input_size, kernel_size, stride, n_fmaps, n_latent),
-    Decoder_4_sampling_bn_1px_deep(output_size, kernel_size, stride, n_fmaps, n_latent))
+    Encoder_4_sampling_bn_1px_deep_convonly_skip(input_size, kernel_size, stride, n_fmaps, n_latent),
+    Decoder_4_sampling_bn_1px_deep_convonly_skip(output_size, kernel_size, stride, n_fmaps, n_latent))
 # loading the model
 checkpoint = torch.load(state_dict_path, map_location=lambda storage, loc: storage)
 state_dict = checkpoint['model_state_dict']
