2d8aaf01ec9a945b1ac36a206dbb1a1b72077392
Ali Karimi <ali.karimi@brain.mpg.de>
2020-09-07 15:49:34 +0200

    Added the graph writer to util.tensorboard module. I was using tensorboardX when the graph did not get plotted. switched to pytorch tensorboard module

--- genEM3/util/tensorboard.py
+++ genEM3/util/tensorboard.py
@@ -1,5 +1,6 @@
 """Tensorboard related general functionalities"""
 from tensorboard import program
+import torch
 
 
 def launch_tb(logdir: str = None, port: str = '7900'):
@@ -9,3 +10,14 @@ def launch_tb(logdir: str = None, port: str = '7900'):
     url = tb.launch()
     print(f'======\nLaunching tensorboard,\nDirectory: {logdir}\nPort: {port}\n======\n')
     return url
+
+
+def add_graph(writer: torch.utils.tensorboard.SummaryWriter = None,
+              model: torch.nn.Module = None,
+              data_loader: torch.utils.data.dataloader = None,
+              device: torch.device = torch.device('cpu')):
+    """Plot the graph of the model"""
+    # get an example image for running through the network
+    input_batch_dict = next(iter(data_loader))
+    input_batch = input_batch_dict['input'].to(device)
+    writer.add_graph(model, input_batch)
